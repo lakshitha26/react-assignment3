6c6e313ad7e29ca5a33c583ab5f49f4943c63261
lakshitha <sailakshitha409@gmail.com>
2023-01-30 13:22:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
+import { faker } from '@faker-js/faker';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App=()=> {
+  return (<center>
+    <table  border="1" class="ui very basic collapsing celled table" style={{marginTop:"5%",}}>
+  <thead>
+    <tr><th>Employee</th>
+    <th>Correct Guesses</th>
+  </tr></thead>
+  <tbody>
+    <tr>
+      <td>
+        <h4 class="ui image header">
+          <img src={faker.image.avatar()} alt=""class="ui mini rounded image"/>
+          <div class="content">
+            Lena
+            <div class="sub header">Human Resources
+          </div>
+        </div>
+      </h4></td>
+      <td>
+        22
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <h4 class="ui image header">
+          <img src={faker.image.avatar()}alt="" class="ui mini rounded image"/>
+          <div class="content">
+            Matthew
+            <div class="sub header">Fabric Design
+          </div>
+        </div>
+      </h4></td>
+      <td>
+        15
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <h4 class="ui image header">
+          <img src={faker.image.avatar()} alt=""class="ui mini rounded image"/>
+          <div class="content">
+            Lindsay
+            <div class="sub header">Entertainment
+          </div>
+        </div>
+      </h4></td>
+      <td>
+        12
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <h4 class="ui image header">
+          <img src={faker.image.avatar()} alt=""class="ui mini rounded image"/>
+          <div class="content">
+            Mark
+            <div class="sub header">Executive
+          </div>
+        </div>
+      </h4></td>
+      <td>
+        11
+      </td>
+    </tr>
+  </tbody>
+</table></center>
   );
 }
 
